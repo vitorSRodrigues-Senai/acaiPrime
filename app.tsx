a484ce6808af6f43149fa9a325c15d9b09327ed7
Vitor <vitor.s.rodrigues@aluno.senai.br>
2026-09-23 15:01:47 -0300

    feat(style): iniciar estilização, adicionar cabeçalho e imagens
    - Inicia a estrutura de estilização do projeto em React
    - Adiciona o texto do header no componente principal
    - Configura e renderiza as imagens iniciais

--- app.tsx
+++ app.tsx
@@ -1,13 +1,37 @@
-import { ScrollView } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import Header from "./components/Header";
 
-export default function App(){
+export default function App() {
 
-    return(
-        <ScrollView>
-         {/* header */}
-          <Header />
-        {/* header */}
-        </ScrollView>
-    )
+  return (
+    <ScrollView>
+    {/* header */}
+      <Header />
+    {/* header */}
+     
+    {/* content */}
+      <View style={styles.content}>
+        <View style={styles.grettingSection}>
+          <Text>
+            
+          </Text>
+
+        </View>
+
+      </View>
+    {/* content */}
+
+    </ScrollView>
+  )
+
 }
+
+  const styles = StyleSheet.create({
+    content: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  })
+ 
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,51 @@
-import { StyleSheet, Text, View } from "react-native";
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { Ionicons } from '@expo/vector-icons';
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function Header() {
     return (
         <View style={styles.header}>
             <View>
-            <Text style={styles.headerTitle}>Acaí Prime</Text>
-            <Text style={styles.header.SubTitle}>O sabor puro da Amazônia</Text>
-
+                <Text style={styles.headerTitle}>Acaí Prime</Text>
+                <Text style={styles.headerSubtitle}>O sabor puro da Amazônia</Text>
             </View>
-        
-        
+
+            <Image source={require('../assets/mulher.jpg')} style={styles.avatarPlaceholder} />
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     header: {
+    width: '100%',
+    paddingHorizontal: 24,
+    paddingTop: 60,
+    paddingBottom: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
 
-    }
+  headerTitle: {
+    fontSize: 24,
+    fontWeight: "800",
+    color: "#2f2d2c"
+  },
+
+  headerSubtitle: {
+    fontSize: 14,
+    marginTop: 4,
+    color: "#9b9b9b"
+  },
+
+  avatarPlaceholder: {
+    width: 44,
+    height: 44,
+    borderRadius: 24,
+    backgroundColor: "#f0f0f0",
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: '#7B1FA2',
+    borderWidth: 2
+  },
 })
